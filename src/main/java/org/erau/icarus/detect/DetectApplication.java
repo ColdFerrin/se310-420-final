9765985fc1afaa409459625e097a1039fbc91db2
coldferrin <ferrinkatz@ferrinkatz.com>
2019-02-01 17:57:41 -0700

    first commit

---
+++ src/main/java/org/erau/icarus/detect/DetectApplication.java
@@ -0,0 +1,14 @@
+package org.erau.icarus.detect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DetectApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/org/erau/icarus/detect/DetectApplicationTests.java
@@ -0,0 +1,17 @@
+package org.erau.icarus.detect;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DetectApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+

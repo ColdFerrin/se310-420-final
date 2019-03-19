cd0b7f360a08efedbcc40ae6d69634b804a5ca1c
coldferrin <ferrinkatz@ferrinkatz.com>
2019-03-19 09:24:52 -0700

    Create constructor for drone storage service and update dependency version.

--- src/main/java/org/erau/icarus/detect/ES/Service/DroneStorageService.java
+++ src/main/java/org/erau/icarus/detect/ES/Service/DroneStorageService.java
@@ -5,39 +5,77 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
 import org.erau.icarus.detect.ES.Model.DroneInfo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
 import java.util.Optional;
 
 @Service
 public class DroneStorageService implements DroneStorageInterface {
 
+    //Create a default json mapper
     private static final ObjectMapper DEFAULT_JSON_MAPPER = new ObjectMapper()
             .registerModule(new JavaTimeModule())
             .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
             .enable(JsonParser.Feature.ALLOW_COMMENTS)
             .findAndRegisterModules();
 
+    private RestHighLevelClient restHighLevelClient;
 
-    @Value("${detect.es.hosts}")
-    private String[] esHosts;
-
-    public DroneStorageService() {
-    }
-
-    @PostConstruct
-    public void init(){
-        HttpHost[] httpHost = new HttpHost[esHosts.length];
+    @Autowired
+    public DroneStorageService(@Value("${detect.es.hosts}") String[] esHosts) {
+        //Parse the hosts in the file application.properties and create the http hosts for the database
+        HttpHost[] httpHosts = new HttpHost[esHosts.length];
+        //loop through hosts
         for (int i = 0; i < esHosts.length; i++){
             String host;
             int port;
             String protocall;
 
+            //parse this host on the semicolons
             String[] parts = esHosts[i].split(":");
+            //if there is only one part then there is only a web address set port and protocol to default
+            if(parts.length == 1){
+                port = 9200;
+                protocall = "http";
+                host = parts[0];
+            }
+            // if there are two parts
+            else if (parts.length == 2) {
+                //check if the first part is http or https and use default port
+                if(parts[0].equalsIgnoreCase("http") || parts[0].equalsIgnoreCase("https")){
+                    protocall = parts[0];
+                    host = parts[1].substring(2);
+                    port = 9200;
+                }
+                //otherwise use the default protocol
+                else {
+                    protocall = "http";
+                    host = parts[0];
+                    port = Integer.parseInt(parts[1]);
+                }
+            }
+            //parse parts if there are 3
+            else if (parts.length == 3){
+                protocall = parts[0];
+                host = parts[1].substring(2);
+                port = Integer.parseInt(parts[2]);
+            }
+            //else use all defaults
+            else {
+                protocall = "http";
+                host = "localhost";
+                port = 9200;
+            }
+            httpHosts[i] = new HttpHost(host, port, protocall);
         }
+        restHighLevelClient = new RestHighLevelClient(
+                RestClient.builder(httpHosts)
+        );
     }
 
     @Override

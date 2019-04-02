e49a997591797e3c6c7ca749e0af702f10cde152
coldferrin <ferrinkatz@ferrinkatz.com>
2019-04-02 10:53:37 -0700

    Add request methods to request mappings.

--- src/main/java/org/erau/icarus/detect/Controllers/InputController.java
+++ src/main/java/org/erau/icarus/detect/Controllers/InputController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,13 +21,13 @@ public class InputController {
         this.bnpIdService = bnpIdService;
     }
 
-    @RequestMapping("/image")
+    @RequestMapping(value = "/image", method = RequestMethod.PUT)
     ResponseEntity<?> inputImage(@RequestBody DroneInfo droneInfo){
         System.out.println("You have uploaded an image");
         return new ResponseEntity<>("You have uploaded an image", HttpStatus.OK);
     }
 
-    @RequestMapping("/update")
+    @RequestMapping(value = "/update", method = RequestMethod.PUT)
     ResponseEntity<?> updateMetadata(){
         System.out.println("You have updated metadata");
         return new ResponseEntity<>("You have updated metadata", HttpStatus.OK);

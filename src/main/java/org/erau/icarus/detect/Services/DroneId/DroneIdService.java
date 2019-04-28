1d610b208dd377f04c3ae507f097753d9c66866b
carrinoc <codycarrino@gmail.com>
2019-04-28 22:35:59 +0000

    Comments Added

--- src/main/java/org/erau/icarus/detect/Services/DroneId/DroneIdService.java
+++ src/main/java/org/erau/icarus/detect/Services/DroneId/DroneIdService.java
@@ -29,26 +29,34 @@ public class DroneIdService {
     public void setRandSeed(Long seed) {
         rand = new Random(seed);
     }
-
+    //Creates DroneInfo input function for DroneIdService
     public DroneInfo input(DroneInfo droneInfo) throws IOException {
+        //Creates an Idenfifications input named input
         Identification input = new Identification();
+        //Calls setIdType from input and passes DroneID as the string to be set
         input.setIdType("DroneID");
+        //Creates an ArrayList of floats named scores
         ArrayList<Float> scores = new ArrayList<>();
 
         //This will be removed when switching to AI
+        //Generated 128 random floats to emulate the 128 dimension analysis
         for(int i = 0; i < 128; i++){
             scores.add(rand.nextFloat());
         }
 
+        //Calls setScore from input and passes the ArrayList scores
         input.setScore(scores);
+        //Creates a float for genData and sets it equal to a random number
         float genData = rand.nextFloat();
         //This will be removed when switching to AI
+        //Needed to emulate that some results need to be reviewed
         if(genData > .9){
             input.setHumanReviewNeeded(true);
         }
-
+        //Calls DroneInfo's getIdentifications() to get the Identifications ArrayList and uses the add function of ArrayLists
+        //to add the Identifications object input to the Identifications ArrayList
         droneInfo.getIdentifications().add(input);
-
+        //Returns the result of the function save from the droneStorageService passing the droneInfo
         return droneStorageService.save(droneInfo);
     }
 }
